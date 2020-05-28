d6670340ef364da913eaf6936aeb78586578f037
while_ine <while_ine>
2020-05-28 17:27:26 +0800

    更新1.10

--- src/com/base/spring/section_1_9_8/valueannotation/javaconfig/AppConfig.java
+++ src/com/base/spring/section_1_9_8/valueannotation/javaconfig/AppConfig.java
@@ -11,10 +11,22 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @PropertySource("classpath:com/base/spring/section_1_9_8/valueannotation/xmlfiles/application.properties")
 public class AppConfig {
 
-//没法启动，先放着 TODO
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    //使用java配置
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext();
+        System.out.println("a");
+        ac.setConfigLocation("com/base/spring/section_1_9_8/valueannotation/xmlfiles/appConfig.xml");
+        System.out.println("b");
+        ac.refresh();
+        System.out.println("c");
+
+        MovieRecommender bean = ac.getBean(MovieRecommender.class);
+        System.out.println(bean);
     }
 
 
