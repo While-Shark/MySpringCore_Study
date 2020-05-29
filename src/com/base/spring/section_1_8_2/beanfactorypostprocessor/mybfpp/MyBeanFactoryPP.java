a633a235bc1e6186109d4ef05e831687266c4cc5
while_ine <while_ine>
2020-05-29 18:06:42 +0800

    更新1.10

--- src/com/base/spring/section_1_8_2/beanfactorypostprocessor/mybfpp/MyBeanFactoryPP.java
+++ src/com/base/spring/section_1_8_2/beanfactorypostprocessor/mybfpp/MyBeanFactoryPP.java
@@ -1,17 +1,20 @@
 package com.base.spring.section_1_8_2.beanfactorypostprocessor.mybfpp;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.core.Ordered;
 
 public class MyBeanFactoryPP implements BeanFactoryPostProcessor, Ordered {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        /*DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
+        DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
         //Person person = (Person) defaultListableBeanFactory.getBean("person");
         BeanDefinition person = defaultListableBeanFactory.getBeanDefinition("person");
-        defaultListableBeanFactory.registerBeanDefinition("person1",person);*/
+        defaultListableBeanFactory.registerBeanDefinition("person1",person);
+
         //lowest value has the highest priority
         System.out.println("order: " + getOrder());
     }
